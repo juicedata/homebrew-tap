1aef761311996e161f11920de0495e7e5724217e
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-10 20:32:17 +0800

    Brew formula update for juicesync version v0.0.4

--- Formula/juicesync.rb
+++ Formula/juicesync.rb
@@ -6,12 +6,12 @@ class Juicesync < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/yujunz/juicesync/releases/download/v0.0.4/juicesync_0.0.4_Darwin_x86_64.tar.gz"
-    sha256 "d2f35d58094a37bd95fa6624a996a389bddbf7c75dafcc818175bd43450e6e1c"
+    url "https://github.com/juicedata/juicesync/releases/download/v0.0.4/juicesync_0.0.4_Darwin_x86_64.tar.gz"
+    sha256 "f83660cc94c7586690d006a3fafa3741aaa2b6db2cf778c9670c354b6227e60d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/yujunz/juicesync/releases/download/v0.0.4/juicesync_0.0.4_Linux_x86_64.tar.gz"
-      sha256 "9dcad86a371e29aa9efec1cb4047fced605485ccb36093378c1015f643233677"
+      url "https://github.com/juicedata/juicesync/releases/download/v0.0.4/juicesync_0.0.4_Linux_x86_64.tar.gz"
+      sha256 "0229f2ad9d3024c32bee064272b717a7f6a512fdb8438649df51e9e4a3e568ab"
     end
   end
 
