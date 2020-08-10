2f25ec5d15c48140dad0d0a53c82b60230689db6
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-10 15:25:23 +0800

    Brew formula update for juicesync version v0.3.2

--- Formula/juicesync.rb
+++ Formula/juicesync.rb
@@ -2,16 +2,16 @@
 class Juicesync < Formula
   desc "A tool to move your data between any clouds or regions."
   homepage "https://juicefs.com/"
-  version "0.3.1"
+  version "0.3.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/juicedata/juicesync/releases/download/v0.3.1/juicesync_0.3.1_Darwin_x86_64.tar.gz"
-    sha256 "22304164bf98160c22bded405c0536e95f85bdce55a154383bbe13bbb7a21cb5"
+    url "https://github.com/juicedata/juicesync/releases/download/v0.3.2/juicesync_0.3.2_Darwin_x86_64.tar.gz"
+    sha256 "6ba707c660f21c8cbd2495383f1dc78f98d670b4489817b916163a62177f5626"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/juicedata/juicesync/releases/download/v0.3.1/juicesync_0.3.1_Linux_x86_64.tar.gz"
-      sha256 "e0179864455c6d6b1d5a8d31d566160b3ab50293b099c84e6c6c7f470b1b9855"
+      url "https://github.com/juicedata/juicesync/releases/download/v0.3.2/juicesync_0.3.2_Linux_x86_64.tar.gz"
+      sha256 "5d2bbb708c6b0c8830fb157ec62f6add35656f865b74191db9c6c8fd54d0af8b"
     end
   end
 
