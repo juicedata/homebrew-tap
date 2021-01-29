857c3fba83eb48ab429f561631a4bf2f4587f155
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-29 22:22:45 +0800

    Brew formula update for juicefs version v0.10.0

--- Formula/juicefs.rb
+++ Formula/juicefs.rb
@@ -9,16 +9,16 @@ class Juicefs < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs_0.10.0_darwin_amd64.tar.gz"
-    sha256 "2fc1fef8331d14717da27cf448a6830a9b9977b0c6d0eb44dc9b3a02b51d51ef"
+    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs-0.10.0-darwin-amd64.tar.gz"
+    sha256 "cae8285abfe87eaff77270c44638d98aa8a25520cd8fdc219d31da02674d77ac"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs_0.10.0_linux_amd64.tar.gz"
-    sha256 "1fefef0fc4af8351e1278f99c95fe371231f9ee46245e27852df0cbb90a8b530"
+    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs-0.10.0-linux-amd64.tar.gz"
+    sha256 "fae9c012d1666f3dcc8442d36daba5de74f116f9ddd141e77e655a22bcd5031b"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs_0.10.0_linux_arm64.tar.gz"
-    sha256 "d2492ae2fbdb05d8f46d9635ece5a334e1f5c947cfd118c45be2fa21da59281a"
+    url "https://github.com/juicedata/juicefs/releases/download/v0.10.0/juicefs-0.10.0-linux-arm64.tar.gz"
+    sha256 "b3a975a57d52ab69c08dc0895a3f5c12e29fc813378982e8a044700ef1f48e68"
   end
 
   def install
