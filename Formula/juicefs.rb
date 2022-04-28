e72003638e1b02ee1faa36607b624773b2929217
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-28 14:10:18 +0800

    Brew formula update for juicefs version v1.0.0-alpha3

--- Formula/juicefs.rb
+++ Formula/juicefs.rb
@@ -10,7 +10,7 @@ class Juicefs < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/juicedata/juicefs/releases/download/v1.0.0-alpha3/juicefs-1.0.0-alpha3-darwin-amd64.tar.gz"
-      sha256 "d07d23dfbf90d292c29f45c434aa20855ed63c95d2921fa661982c3fcdd24494"
+      sha256 "cfe55ca99f870a957008170910f5de291339eb744b477c13e3ef626c9dd19d2f"
 
       def install
         bin.install "juicefs"
@@ -18,7 +18,7 @@ class Juicefs < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/juicedata/juicefs/releases/download/v1.0.0-alpha3/juicefs-1.0.0-alpha3-darwin-arm64.tar.gz"
-      sha256 "65f2ecf5cec0bae134835717dd47dc503e9d1786829ade6b1890b3c5a2dcc5e3"
+      sha256 "54d1a9d3a1af51a3c3dbf827057fa437c5735653cc38e5ba4fb2f812f140914a"
 
       def install
         bin.install "juicefs"
@@ -29,7 +29,7 @@ class Juicefs < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/juicedata/juicefs/releases/download/v1.0.0-alpha3/juicefs-1.0.0-alpha3-linux-arm64.tar.gz"
-      sha256 "528a7d5170720690a846aa30549396fc5d6297723f2b577d83108f2d5d49412f"
+      sha256 "59b11eaad672f4d99a5565d8812a2cfdd0d91aee0fc2ea7a27836449ade6713a"
 
       def install
         bin.install "juicefs"
@@ -37,7 +37,7 @@ class Juicefs < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/juicedata/juicefs/releases/download/v1.0.0-alpha3/juicefs-1.0.0-alpha3-linux-amd64.tar.gz"
-      sha256 "dea25d1a148ce42eacb11b37ad5d328d2732411325c265a3e115edbe31f96ee4"
+      sha256 "18c1e70095420a8f9af1a5a464673926704f3ae1567e81ccbc7a93209437181a"
 
       def install
         bin.install "juicefs"
